52fcf5a7a87aac11655af732bcdaa6a9eb6c7e55
shaharsolutions <shaharsolutions@gmail.com>
2026-03-23 21:01:32 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,8 +2,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Safety check for environments without Vite (like VS Code Live Server / GitHub Pages)
 const env = import.meta.env || {}
-const supabaseUrl = env.VITE_SUPABASE_URL || 'https://czfjbmkjnodonmtjvwep.supabase.co'
-const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZmpibWtqbm9kb25tdGp2d2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjA5MzQsImV4cCI6MjA4ODUzNjkzNH0.R8syO-AS9CcIrP3tYBFO9PTs388UG7rs6SCoVx1Sb4A'
+const supabaseUrl = env.VITE_SUPABASE_URL || 'https://iduyexkzivtnvrdsbwig.supabase.co'
+const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkdXlleGt6aXZ0bnZyZHNid2lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NjYwMTYsImV4cCI6MjA4OTA0MjAxNn0.MhqZwvY7RiOBBqgBhRD-e-SqbI7NIf2vWxNuD5_6e48'
 
 // This initializes the Supabase client
 export const supabase = (supabaseUrl && supabaseAnonKey) 
